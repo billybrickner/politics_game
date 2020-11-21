51b3b2db4c7e9c4e21eb0cd3f4b84bfb7a9b9663
Billy Brickner <billybrickner@gmail.com>
2020-11-21 10:52:09 -0500

    Updated to circular boundaries and a few other updates for cleanliness

--- manyToMany.py
+++ manyToMany.py
@@ -12,11 +12,13 @@ os.environ["SDL_VIDEO_CENTERED"] = "1"
 
 SCREEN_SIZE = 600
 screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
-pygame.display.set_caption("LEVEL 2 = Find the Correct Square!")
+pygame.display.set_caption("Politics Game: Data Display")
 
 clock = pygame.time.Clock()
 
-def genHSLColor():
+################################################################################
+# Generate a random hue
+def genHue():
     triangle = np.random.randint(3)
     randScale = np.random.rand()
     if triangle == 0:
@@ -26,88 +28,97 @@ def genHSLColor():
     if triangle == 2:
         return (255*randScale, 0, 255*(1-randScale))
 
+def genUnitVector():
+    angle = (2*np.pi)*np.random.rand()
+    return np.array([np.sin(angle), np.cos(angle)])
+
+def reflectNorm(current_direction, normal):
+    mag = np.linalg.norm(normal)
+    projection = np.dot(current_direction, normal)/(mag**2)*normal
+    current_direction -= 2*projection
+
+################################################################################
+# A class for particle system categories.
 class Categories(object):
     def __init__(self, num_categories, num_points):
         self.num_categories = num_categories
         self.locations = np.random.uniform(0, SCREEN_SIZE, size=(num_categories,2))
         self.directions = np.zeros(shape=(num_categories,2))
-        for cat in range(num_categories):
-            angle = 2*np.pi*np.random.rand()
-            self.directions[cat][0] = np.sin(angle)
-            self.directions[cat][1] = np.cos(angle)
-        self.colors = [genHSLColor() for color in range(num_categories)]
+        # Give each category a random direction
+        for category in range(num_categories):
+            self.directions[category] += genUnitVector()
+        self.colors = [genHue() for color in range(num_categories)]
         self.point_totals = [0 for _ in range(num_categories)]
         self.points = [DataPoint(self) for _ in range(num_points)]
-        self.threshold = 0.2
+        self.flow_rate = 0.1
 
-    def getCategory(self, init=False):
-        cat = np.random.randint(self.num_categories)
+    def getCategory(self, category):
+        init = (category == -1)
+        category = np.random.randint(self.num_categories)
         if init:
-            self.point_totals[cat] += 1
-        return cat
+            self.point_totals[category] += 1
+        return category
 
-    def getArea(self, category):
-        return  self.point_totals[category]/5
+    def getSize(self, category):
+        return  np.sqrt(self.point_totals[category]) * 3
 
-    def getNextDest(self, category, location):
-        area = self.getArea(category)
-        thresh = self.threshold
-        angle = np.random.rand()*2*np.pi
-        offset = np.array([np.sin(angle), np.cos(angle)])
-        if np.linalg.norm(location - self.locations[category])/area < thresh:
-            category = self.getCategory()
-            offset = offset*thresh*area*1.2
+    def getNextDest(self, point, location):
+        category = point.category
+        size = self.getSize(category)
+        flow_rate = self.flow_rate
+        offset = genUnitVector()
+        # The probability is calculated based on the probability of dot being a
+        # particular distance from the center
+        flow_probability =  np.linalg.norm(location - self.locations[category])/size
+        # Make the next point go to a new category, but don't trigger the flow
+        # probability at the next destination
+        flow_margin = 1.01
+        if flow_probability < flow_rate:
+            category = self.getCategory(category)
+            offset = offset*size*(flow_rate*flow_margin)
         else:
-            offset = offset*np.random.rand()*area
-        return (self.locations[category] + offset, category)
+            # The random value for deciding the flow probability is chosen here
+            # The effect is that points close to the center get sent to other
+            # categories
+            offset = offset*np.random.rand()*size
+        point.category = category
+        return self.locations[category] + offset
 
     def getColor(self, category):
         return self.colors[category]
 
-    def reflectNorm(self, category, normal):
-        current_direction = self.directions[category]
-        mag = np.linalg.norm(normal)
-        projection = np.dot(current_direction, normal)/(mag**2)*normal
-        current_direction -= 2*projection
-
     def collisions(self):
+        radius = SCREEN_SIZE/2
+        center = np.array([SCREEN_SIZE/2, SCREEN_SIZE/2])
         for category in range(self.num_categories):
-            cur_loc = self.locations[category]
-            cur_area = self.getArea(category)
-            # Check collision with other category areas
-            for otherCat in range(self.num_categories):
+            cur_loc = self.locations[category]  # Current Location
+            cur_size = self.getSize(category)   # Current Size
+            marg = 1.01                         # Margin to separate collision
+            # Check collision with other category sizes
+            for otherCat in range(category, self.num_categories):
                 if otherCat != category:
                     other_loc = self.locations[otherCat]
-                    other_area = self.getArea(otherCat)
+                    other_size = self.getSize(otherCat)
                     cur_distance = np.linalg.norm(cur_loc - other_loc)
-                    exp_distance = cur_area +  other_area
+                    exp_distance = cur_size +  other_size
                     if cur_distance <= exp_distance:
                         mov_dir = cur_loc - other_loc
-                        cur_loc += mov_dir/cur_distance*(exp_distance-cur_distance)*1.01
-                        self.reflectNorm(category, cur_loc - other_loc)
-                        self.reflectNorm(otherCat, other_loc - cur_loc)
-            if cur_loc[0] <= cur_area:
-                cur_loc[0] = cur_area*1.01
-                self.reflectNorm(category, np.array([1,0]))
-            if cur_loc[0] + cur_area >= SCREEN_SIZE:
-                cur_loc[0] = SCREEN_SIZE - cur_area*1.01
-                self.reflectNorm(category, np.array([-1,0]))
-            if cur_loc[1] <= cur_area:
-                cur_loc[1] = cur_area*1.01
-                self.reflectNorm(category, np.array([0,1]))
-            if cur_loc[1] + cur_area >= SCREEN_SIZE:
-                cur_loc[1] = SCREEN_SIZE - cur_area*1.01
-                self.reflectNorm(category, np.array([0,-1]))
+                        cur_loc += mov_dir/cur_distance*(exp_distance-cur_distance)*marg
+                        reflectNorm(self.directions[category], cur_loc - other_loc)
+                        reflectNorm(self.directions[otherCat], other_loc - cur_loc)
+            normal = center - cur_loc
+            center_distance = np.linalg.norm(normal)
+            normal = normal/center_distance
+            if center_distance + cur_size >= radius:
+                normal = normal*(center_distance + cur_size - radius)*marg
+                cur_loc += normal
+                reflectNorm(self.directions[category], normal)
 
     def updateLocations(self, screen):
-        self.threshold = max(0.01, 0.15 * np.sin(pygame.time.get_ticks()*2*np.pi/1000/10))
+        self.flow_rate = max(0.01, 0.15 * np.sin(pygame.time.get_ticks()*2*np.pi/1000/10))
         self.collisions()
-        for cat in range(self.num_categories):
-            speed = 0.5
-            direction = self.directions[cat]
-            new_loc = self.locations[cat] + direction*speed
-            for i in range(len(self.locations[cat])):
-                self.locations[cat][i] = new_loc[i]
+        speed = 1
+        self.locations += self.directions*speed
         for point in self.points:
             point.draw(screen)
 
@@ -117,7 +128,7 @@ class DataPoint(object):
         self.dest = np.array([self.rect.x, self.rect.y])
         self.gridSize = 24
         self.categories = categories
-        self.category = categories.getCategory(init=True)
+        self.category = categories.getCategory(-1)
         self.color = categories.getColor(self.category)
 
     def handle_keys(self):
@@ -136,13 +147,13 @@ class DataPoint(object):
            sys.exit()
 
     def draw(self, surface):
-        loc = np.array([self.rect.x, self.rect.y])
-        direction = self.dest - loc
+        location = np.array([self.rect.x, self.rect.y])
+        direction = self.dest - location
         mag = np.linalg.norm(direction)
         if (mag < 1):
             self.color = self.categories.getColor(self.category)
-            self.dest, self.category = self.categories.getNextDest(self.category, loc)
-        max_speed = 3
+            self.dest = self.categories.getNextDest(self, location)
+        max_speed = 10
         if mag > 2*max_speed:
             direction = max_speed/mag*direction
         self.rect.move_ip(round(direction[0]), round(direction[1]))
